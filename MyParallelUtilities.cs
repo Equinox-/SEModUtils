cfc51deea95a39c7551cd0e68ddccddee162d2a8
Westin Miller <equinoxscripts@gmail.com>
2017-08-09 17:30:55 -0700

    Removed last reference to SessionCore. This should be able to stand alone w.r.t ProceduralWorld.

--- MyParallelUtilities.cs
+++ MyParallelUtilities.cs
@@ -4,20 +4,10 @@ using VRage;
 
 namespace Equinox.ProceduralWorld.Utils
 {
-    public class MyPriorityParallel
+    public class MyParallelUtilities
     {
         public static void InvokeOnGameThreadBlocking(Action action)
         {
-            // Better way in ModAPI?
-            Exception trace = null;
-            try
-            {
-                int.Parse("fail");
-            }
-            catch (Exception e)
-            {
-                trace = e;
-            }
             var mutex = new FastResourceLock();
             mutex.AcquireExclusive();
             MyAPIGateway.Utilities.InvokeOnGameThread(() =>
@@ -25,10 +15,6 @@ namespace Equinox.ProceduralWorld.Utils
                 try
                 {
                     action();
-                }
-                catch (Exception e)
-                {
-                    SessionCore.Log("Background task failed. Reason:\n{0}\nInvoked From:\n{1}", e, trace);
                 }
                 finally
                 {
